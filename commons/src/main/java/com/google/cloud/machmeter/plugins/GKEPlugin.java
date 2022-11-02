57cef1520aaed24b0cec92311330b24e3d536e72
Manit Gupta <manitgupta@google.com>
2022-11-02 11:33:17 +0530

    Models for the Terraform plugin

---
+++ commons/src/main/java/com/google/cloud/machmeter/plugins/GKEPlugin.java
@@ -0,0 +1,4 @@
+package com.google.cloud.machmeter.plugins;
+
+public class GKEPlugin {
+}
---
+++ commons/src/main/java/com/google/cloud/machmeter/plugins/SpannerInstancePlugin.java
@@ -0,0 +1,16 @@
+package com.google.cloud.machmeter.plugins;
+
+import com.google.cloud.machmeter.model.MachmeterConfig;
+
+public class SpannerInstancePlugin implements PluginInterface {
+
+  @Override
+  public String getName() {
+    return "spannerInstancePlugin";
+  }
+
+  @Override
+  public void execute(MachmeterConfig config) {
+
+  }
+}
