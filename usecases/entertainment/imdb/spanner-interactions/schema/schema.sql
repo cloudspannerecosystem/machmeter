619d1fe0a4998d390d7b56f410cf642f76f62837
Manit Gupta <manitgupta@google.com>
2022-11-21 13:36:33 +0530

    Add entertainment/imdb template

---
+++ usecases/entertainment/imdb/spanner-interactions/schema/schema.sql
@@ -0,0 +1,69 @@
+CREATE TABLE actors (
+  id INT64 NOT NULL,
+  first_name STRING(100),
+  last_name STRING(100),
+  gender STRING(1),
+) PRIMARY KEY(id);
+
+CREATE INDEX actors_first_name ON actors(first_name);
+
+CREATE INDEX actors_last_name ON actors(last_name);
+
+CREATE TABLE directors (
+  id INT64 NOT NULL,
+  first_name STRING(100),
+  last_name STRING(100),
+) PRIMARY KEY(id);
+
+CREATE INDEX directors_first_name ON directors(first_name);
+
+CREATE INDEX directors_last_name ON directors(last_name);
+
+CREATE TABLE directors_genres (
+  director_id INT64 NOT NULL,
+  genre STRING(100) NOT NULL,
+  prob FLOAT64,
+  CONSTRAINT directors_genres_ibfk_1 FOREIGN KEY(director_id) REFERENCES directors(id),
+) PRIMARY KEY(director_id, genre);
+
+CREATE INDEX directors_genres_director_id ON directors_genres(director_id);
+
+CREATE TABLE movies (
+  id INT64 NOT NULL,
+  name STRING(100),
+  year INT64,
+  rank FLOAT64,
+) PRIMARY KEY(id);
+
+CREATE INDEX movies_name ON movies(name);
+
+CREATE TABLE movies_directors (
+  director_id INT64 NOT NULL,
+  movie_id INT64 NOT NULL,
+  CONSTRAINT movies_directors_ibfk_1 FOREIGN KEY(director_id) REFERENCES directors(id),
+  CONSTRAINT movies_directors_ibfk_2 FOREIGN KEY(movie_id) REFERENCES movies(id),
+) PRIMARY KEY(director_id, movie_id);
+
+CREATE INDEX movies_directors_director_id ON movies_directors(director_id);
+
+CREATE INDEX movies_directors_movie_id ON movies_directors(movie_id);
+
+CREATE TABLE movies_genres (
+  movie_id INT64 NOT NULL,
+  genre STRING(100) NOT NULL,
+  CONSTRAINT movies_genres_ibfk_1 FOREIGN KEY(movie_id) REFERENCES movies(id),
+) PRIMARY KEY(movie_id, genre);
+
+CREATE INDEX movies_genres_movie_id ON movies_genres(movie_id);
+
+CREATE TABLE roles (
+  actor_id INT64 NOT NULL,
+  movie_id INT64 NOT NULL,
+  role STRING(100) NOT NULL,
+  CONSTRAINT roles_ibfk_1 FOREIGN KEY(actor_id) REFERENCES actors(id),
+  CONSTRAINT roles_ibfk_2 FOREIGN KEY(movie_id) REFERENCES movies(id),
+) PRIMARY KEY(actor_id, movie_id, role);
+
+CREATE INDEX actor_id ON roles(actor_id);
+
+CREATE INDEX movie_id ON roles(movie_id);
