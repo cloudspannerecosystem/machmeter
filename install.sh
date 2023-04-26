d11094b25399c005bcd935d420f0c0b0830e6b42
Kyle Insaurralde <34525787+kinsaurralde@users.noreply.github.com>
2023-04-25 21:33:56 -0700

    Create install script to install dependencies (#73)
    Signed-off-by: Kyle Insaurralde <34525787+kinsaurralde@users.noreply.github.com>

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl
+sudo apt-get install wget
+sudo apt-get install git
+sudo apt-get install -yq openjdk-11-jdk git maven
+
+# Install terraform.
+wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update && sudo apt install terraform
+
+# Install kubectl.
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+kubectl version --client
+
